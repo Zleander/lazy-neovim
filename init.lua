637778f9a860cab9cc640c0627e4461a1924f749
Leander Zimmermann <leander.zimmermann@meditech-ai.de>
2026-05-27 12:29:42 +0200

    autotrust .nvim.lua in worktrees dir

--- init.lua
+++ init.lua
@@ -3,4 +3,13 @@ require("config.lazy")
 
 vim.o.exrc = true
 
+local worktrees_root = vim.fn.expand("~/Documents/Work/automedic/automedic-worktrees")
+local cwd = vim.fn.getcwd()
+if cwd:sub(1, #worktrees_root) == worktrees_root then
+  local project_config = cwd .. "/.nvim.lua"
+  if vim.fn.filereadable(project_config) == 1 then
+    pcall(vim.secure.trust, { action = "allow", path = project_config })
+  end
+end
+
 -- set language to en_US.UTF-8
